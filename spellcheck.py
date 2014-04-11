e0dc91438726359b8db07da8642c4af0cdb4f31d
cooltoast <mpatel678@gmail.com>
2014-04-11 00:00:09 -0400

    ran some tests, added duplicate/sort function but need to optimize it

--- spellcheck.py
+++ spellcheck.py
@@ -1,6 +1,104 @@
+from bisect import bisect_left
+
 with open("/usr/share/dict/words") as dictionary:
-    library = dictionary.read().lower().splitlines()
-print library[100143], len(library)
+  library = dictionary.read().lower().splitlines()
+print "Dictionary initialized with length", len(library)
+
+#returns index of x in a_list
+def binary_search(a_list, x, lo=0, hi=None):
+  hi = hi if hi is not None else len(a_list)    
+  pos = bisect_left(a_list,x,lo,hi)          
+  return (pos if pos != hi and a_list[pos] == x else -1)
+
+
+#checks for duplicates and removes them and replaces all vowels with 'a'
+def duplicates(w, v):
+  import itertools
+  import re
+  #duplicates
+  noDup = ''.join(ch for ch, _ in itertools.groupby(w))
+  #vowels
+  allNew = re.sub("[aeiou]", "a", noDup)
+  return allNew
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+vowels = ('aeiou')
+prompt = 'Enter a word: '
+
+#O(n^2), but necessary to make lookup time 
+#O(log(n)) for binary search
+def clear_duplicates_and_sort(a_list):
+  for word in library:
+    while (library.count(word) > 1):
+      library.remove(word)
+  #a_list.sort()    
+  return a_list  
+
+#~~~~~~method below finds index of word in dictionary list~~~~~
+#~~~~~~work toward spell suggestions~~~~~~~~~~~~~~~~~~~~~
+
+
+#no upper case words
+word = raw_input(prompt).lower()
+print word
+
+index = binary_search(library, word)
+
+print "word appears: ", library.count(word)
+
+print "prev word: ", library[index - 1]
+print "word: ", library[index]
+print "next word: ", library[index + 1]
+
+print "prev word: ", library[library.index(word) - 1]
+print "word: ", library[library.index(word)]
+print "next word: ", library[library.index(word) + 1]
+
+print "removing duplicates..."
+
+library = clear_duplicates_and_sort(library)
+
+
+print "word appears: ", library.count(word)
+
+print "prev word: ", library[index - 1]
+print "word: ", library[index]
+print "next word: ", library[index + 1]
+
+print "prev word: ", library[library.index(word) - 1]
+print "word: ", library[library.index(word)]
+print "next word: ", library[library.index(word) + 1]
+
+
+
+'''
+print "index is: ", index
+print "index is: ", library.index(word)
+'''
+#assert binary search works
+#Python index function is O(n)
+#Binary search is O(log(n))
+#reference: https://wiki.python.org/moin/TimeComplexity
+if (index == library.index(word)):
+	print "It worked!"
+
+#~~~~~~method above finds index of word in dictionary list~~~~~
+#~~~~~~work toward spell suggestions~~~~~~~~~~~~~~~~~~~~~
+
+#gets new word with no duplicates/replaced vowels
+newWord = duplicates(word, "aeiou")
+
+#searches for new word in dictionary + calculates Lev + provides suggestion
+new_index = binary_search(library, newWord)
+print newWord
+
+
+'''
+a_list = [0,1,2,3,4,5,6,7,8,9]
+#binary_search test
+print binary_search(a_list, 5)
+'''
+
 
 """
 keeps /n
@@ -8,10 +106,10 @@ keeps /n
 dictionary = open("/usr/share/dict/words")
 library = dictionary.readlines()
 """
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-vowels = ('aeiou')
-word = input("> ").lower()
-print word
+
+
+
+
 '''
 #set up trie data structure
 class trieStruc:
@@ -36,7 +134,7 @@ for one in library:
 
 print trie
 '''
-
+'''
 #checks for duplicates and removes them and replaces all vowels with 'a'
 def duplicates(w, v):
 	import itertools
@@ -77,3 +175,4 @@ newWord = duplicates(word, "aeiou")
 #searches for new word in dictionary + calculates Lev + provides suggestion
 binarySearch()
 print newWord
+'''
