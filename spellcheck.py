7455142d9f038c022286cc8ac516391294707336
cooltoast <mpatel678@gmail.com>
2014-04-12 21:19:20 -0400

    duplicates function is messing up, fixing it

--- spellcheck.py
+++ spellcheck.py
@@ -95,6 +95,15 @@ new_index = binary_search(library, newWord)
 print newWord
 
 
+newWord = duplicates(newWord, "aeiou")
+
+
+print newWord
+
+for letter in newWord:
+  if (letter != 'a'):
+    print "duplicates failed"
+
 '''
 a_list = [0,1,2,3,4,5,6,7,8,9]
 #binary_search test
